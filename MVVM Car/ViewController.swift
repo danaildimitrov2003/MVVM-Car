247216363a78e6ddf0048b96a72604148f15dea7
dvny <danaildimitrov2003@gmail.com>
2023-05-10 10:00:31 +0300

    Initial Commit

---
+++ MVVM Car/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Car
+//
+//  Created by Danail Dimitrov on 10.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
